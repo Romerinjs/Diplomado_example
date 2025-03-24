b61b5e674ec2fd16407661aa4f26cf7c5c23c2fb
Romerinjs <raalmeidab22@itp.edu.co>
2025-03-24 09:10:32 -0500

    TASK-2-Agregando-modulo-autenticacion

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 // Vamos a hacer un comentario aqui para que el chismoso que está viendo este comentario
+function greet(name: string): string {
+    return `Hola, ${name}! Bienvenido al diplomado.`;
+}
+
+console.log(greet("Estudiante"));
