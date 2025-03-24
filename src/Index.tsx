57665daf44c37cffa60138e9d60abd7198109d21
Romerinjs <raalmeidab22@itp.edu.co>
2025-03-24 11:03:15 -0500

    Corrección de bug

--- src/Index.tsx
+++ src/Index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App: React.FC = () => {
-    const user = undefined; // Simulando un error de producción
+    const user: { name?: string } | null = null; // Simulando un error de producción
 
     // Esto generará un error porque `user` es undefined
-    return <div>Hola, {user.name}</div>;
+    return <div>Hola, {user?.name ?? 'Invitado'}</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
