7b36344b874430408b2bbb4820c2ab41fe3e39b0
Romerinjs <raalmeidab22@itp.edu.co>
2025-03-24 10:54:27 -0500

    Cambios de fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // Vamos a hacer un comentario aqui para que el chismoso que está viendo este comentario
-<<<<<<< HEAD
 function greet(name: string): string {
     return `Hola, ${name}! Bienvenido al diplomado.`;
 }
@@ -10,12 +9,5 @@ function farewell(name: string): string {
 }
 
 console.log(farewell("Estudiante"));
-=======
-// chismoso usted hp
-console.log('Hola sapo');
-<<<<<<< HEAD
->>>>>>> feature/Esteban
-=======
-console.log('Hola pyce');
-console.log('Rudzy es un gay');
->>>>>>> feature/Nicko
+// Todos los cambios han sido realizados correctamente
+console.log('Ahora todo está funcional amiguitos para poder lanzar la versión 1.0')
