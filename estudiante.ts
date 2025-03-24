38beb38278dcf3c58cba58fa6019ef83117c6e02
Rudzy302 <rudzy302@gmail.com>
2025-03-24 11:23:30 -0500

    Cambios

--- estudiante.ts
+++ estudiante.ts
@@ -1,2 +1,3 @@
 // Aquí vamos a hacer las funcionalidades del modulo usuario en la sección estudiantes
 // Vamos a hacer un comentario aquí para que el chismoso que está viendo este comentario
+// toca meter un nuevo comentario en la sección de estudiantes
